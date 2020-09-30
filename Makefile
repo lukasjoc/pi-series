07aca89a3f5a41f314167fd9ad7e30b7cdf2d568
Doug Nelson <dougnelson@silktide.com>
2020-09-30 23:09:51 +0100

    added rust support

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-all: ts perl java php js python go c nim v
+all: ts perl java php js python go c nim v rust
 
 .PHONY: all
 
@@ -32,6 +32,9 @@ v:
 
 php:
 	php pi.php
+
+rust:
+	rustc pi.rs; ./pi; rm -f pi
 
 runlog:
 	make all > runlog.txt
---
+++ pi.rs
@@ -0,0 +1,37 @@
+
+fn pi_nks(limit: u64) -> f64 {
+    let mut pi = 3.0;
+    let mut sign = 1;
+
+    let mut i : u64 = 2;
+    while i <= limit {
+        pi += (sign * 4) as f64 / (i * (i + 1) * (i + 2)) as f64;
+        sign = sign * -1;
+        i += 2;
+    }
+
+    pi
+}
+
+
+fn pi_gls(limit: u64) -> f64 {
+    let limit = limit as f64;
+
+    let mut pi = 0.0;
+    let mut divisor= 1.0;
+    let mut sign = 1.0;
+
+    while divisor <= limit {
+        pi += (4.0 / divisor) * sign;
+        sign = -sign;
+        divisor += 2.0;
+    }
+
+    pi
+}
+
+fn main() {
+    let limit: u64 = 10000;
+    println!("NKS: {0:.13}", pi_nks(limit));
+    println!("GLS: {0:.13}", pi_gls(limit));
+}
